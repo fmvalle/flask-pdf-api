89316edabba79c84b22bca557e060ef90a81d11e
Felipe Valle <fmvalle@gmail.com>
2025-05-28 22:00:01 -0300

    atualização inicial

---
+++ api/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_file, abort, request
+import os
+
+app = Flask(__name__)
+
+PDF_FILE_PATH = os.path.abspath("pdfs/matematica_paginas_1_a_26.pdf")
+
+@app.route("/pdf")
+def serve_pdf():
+    try:
+        return send_file(
+            PDF_FILE_PATH,
+            mimetype='application/pdf',
+            as_attachment=False,
+            download_name='matematica.pdf',
+            conditional=True
+        )
+    except Exception as e:
+        print("Erro ao servir PDF:", e)
+        return abort(500)
+
+if __name__ == "__main__":
+    app.run(debug=True)
